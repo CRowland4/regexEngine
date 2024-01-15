30d05fa10bba1ed1fee3827142ef78ccc956a397
Caleb Rowland <cjrowland4@gmail.com>
2024-01-15 13:50:46 -0600

    Commit part of stage 5

--- main.go
+++ main.go
@@ -34,6 +34,17 @@ func isEqualLengthRegexMatch(regex, input string) (isMatch bool) {
 		return result == "true"
 	}
 
+	if strings.Index(regex, "?") == 1 {
+		return handleQuestionMark(regex, input)
+	}
+	if strings.Index(regex, "*") == 1 {
+		return handleAsterisk(regex, input)
+	}
+
+	if strings.Index(regex, "+") == 1 {
+		return handlePlus(regex, input, false)
+	}
+
 	if !isCharacterMatch(regex[0], input[0]) {
 		return false
 	}
@@ -41,7 +52,49 @@ func isEqualLengthRegexMatch(regex, input string) (isMatch bool) {
 	return isEqualLengthRegexMatch(regex[1:], input[1:])
 }
 
+func handlePlus(regex, input string, plusFlag bool) (isMatch bool) {
+	if isBaseCaseMet, result := isBaseCase(regex, input); isBaseCaseMet {
+		return result == "true"
+	}
+	if !isCharacterMatch(regex[0], input[0]) && !plusFlag {
+		return false
+	}
+
+	if !isCharacterMatch(regex[0], input[0]) && plusFlag {
+		regex = strings.Replace(regex, regex[0:2], "", 1)
+		return isEqualLengthRegexMatch(regex, input)
+	}
+
+	// Characters match
+	return handlePlus(regex, input[1:], true)
+
+}
+
+func handleAsterisk(regex, input string) (isMatch bool) {
+	if !isCharacterMatch(regex[0], input[0]) {
+		regex = strings.Replace(regex, regex[0:2], "", 1)
+		return isEqualLengthRegexMatch(regex, input)
+	}
+
+	return isEqualLengthRegexMatch(regex, input[1:])
+}
+
+func handleQuestionMark(regex, input string) (isMatch bool) {
+	if isCharacterMatch(regex[0], input[0]) {
+		input = input[1:]
+	}
+
+	regex = strings.Replace(regex, regex[0:2], "", 1)
+	return isEqualLengthRegexMatch(regex, input)
+}
+
 func isBaseCase(regex, input string) (isMet bool, result string) {
+	if (regex == ".*") && (input == "") {
+		return true, "true"
+	}
+	if (regex == ".+") && (input == "") {
+		return true, "true"
+	}
 	if regex == "" {
 		return true, "true"
 	}
